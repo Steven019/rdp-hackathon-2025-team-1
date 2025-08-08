2ebad842356953d57a9533a3a7e45817016f078e
Steven <steven.m.darragh@sherwin.com>
2025-08-08 14:25:05 +0000

    search matching update

--- streamlit_app/pages/welcome.py
+++ streamlit_app/pages/welcome.py
@@ -43,17 +43,23 @@ def main():
 
     #st.text_input("Search by",width=500)
     column_select = st.multiselect("Search in column", options=column_names, width=400)
-    item_searching = st.text_input("Search for",width=400)
+    item_searching = st.text_input("Search for", width=400)
 
+    filtered_table = main_table.copy()
     if st.button("Search"):
-        data = main_table[column_select]
-        print(data)
+        if column_select and item_searching:
 
-        if item_searching in data:
-            print("Cool Times")
+            match_row = filtered_table[column_select].apply(lambda row: row.astype(str).str.contains(item_searching, case=False, na=False), axis=1)
+
+            if isinstance(match_row, pd.DataFrame):
+                match_row = match_row.any(axis=1)
+            filtered_table = filtered_table[match_row]
+            st.success(f"Found {filtered_table.shape[0]} matching rows.")
         else:
-            print("Bad Times")
-        pass
+            st.warning("Please select at least one column and enter a search term.")
+        st.dataframe(filtered_table, hide_index=True)
+    else:
+        st.dataframe(main_table, hide_index=True)
 
     st.dataframe(main_table, hide_index=True)
 
